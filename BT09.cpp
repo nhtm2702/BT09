1cc6a4a1cd2e53f5ccb0961c4277c26b4c8bd508
nhtm2702 <78303807+nhtm2702@users.noreply.github.com>
2021-03-24 14:52:31 +0700

    BT09_A3.cpp

--- BT09.cpp
+++ BT09.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int strlen(char *s)
-{
-    int i=0;
-    while(s[i] != '\0') i++;
-    return i;
-}
-char* concat(const char* s1, const char* s2)
-{
-    char *kq;
-    int size1 = strlen(s1);
-    int size2 = strlen(s2);
-    strncpy(kq,s1,size1);
-    strncat(kq,s2,size2);
-    cout << kq << endl;
-    return kq;
-}
 int main()
 {
-    char *s1="hello";
-    char *s2="world";
-    char *kqua;
-    kqua = concat(s1,s2);
-    cout << &kqua << endl;
-    for (char *cp = kqua; (*cp) != '\0'; cp++)
-      cout << (void*) cp << " : " << (*cp) << endl;
+    char* a = new char[10];
+    char* c = a + 3;
+    cout << &a << " " << &c << endl;
+    for (int i = 0; i < 9; i++) a[i] = 'a';
+    a[9] = '\0';
+    cerr <<"a: " << "-" << a << "-" << endl;
+    cerr <<"c: " << "-" << c << "-" << endl;
+    delete c;
+    cerr << "a after deleting c:" << "-" << a << "-" << endl; // lenh cerr nay chay khong nhu mong muon vi toan hang delete khong thu duoc gia tri con tro c nen chua xoa duoc c
 }
